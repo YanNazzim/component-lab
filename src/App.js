b5292437398be6a6ebfaa0bdabbc60fe716b917d
Yan <ygonz42@gmail.com>
2024-02-17 03:29:12 -0500

    Finished Lab

---
+++ src/App.js
@@ -0,0 +1,14 @@
+function Heading() {
+  return <h1>This is an h1 heading</h1>
+}
+
+function App() {
+  return ( 
+    <div className="App">
+      I have completed this lab!
+      <Heading /> 
+    </div> 
+  ); 
+} 
+ 
+export default App;
